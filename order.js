239b7471e935efc907d5d7d645edf415c6e27f07
Seth-Wr <168258012+Seth-Wr@users.noreply.github.com>
2024-07-17 03:42:49 -0500

    Number auth with regex

--- order.js
+++ order.js
@@ -2,8 +2,13 @@ const shopping_bag_qty = document.querySelector(".shopping-bag-qty")
 const order = document.querySelector(".order")
 const total_Qty = document.querySelector(".total-qty")
 const checkout_Price = document.querySelector(".checkout-price")
-
-
+const emailPattern = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+const phonePattern = /^(\+?1 *[ -.])?(\d{3}) *[ .-]?(\d{3}) *[ .-]?(\d{4}) *$/
+const email_Input = document.querySelector(".email-input")
+const number_Input = document.querySelector(".number-input")
+const shipping_Input = document.querySelector(".shipping-input")
+const apt_Number_Input = document.querySelector(".apt-num-input")
+const checkout_Btn = document.querySelector(".checkout-btn") 
 
 checkCookie("cart")
 pageReload()
@@ -46,18 +51,55 @@ function render_Page(){
     else {
         checkout_Price.textContent = '$' + cart.totalPrice
     }
-    const html = order_List.toString()
+    const html = `<h3>My Order</h3> ` + order_List.toString()
+    //remove commas from the array to string 
     const finalhtml = html.replace(/,/g, "")
     total_Qty.textContent = cart.totalQty +" Items"
-   
+   //update the dom 
     order.innerHTML = finalhtml
   
   }
 }
 
 
+
+//check all necessary form fields are put in then send api call to notify seller then redirect page
+checkout_Btn.addEventListener('click', ()=>{
+    
+    //regex to verify
+    if(!phonePattern.test(number_Input.value)){
+   
+    }
+    else{
+        
+      
+    }
+
+})
+
+number_Input.addEventListener('keydown', (e)=>{
+// regular expression to only allow numbers and backspaces and spaces dashes periods and deletes
+if(!(/[0-9]|Backspace|ArrowLeft|ArrowRight|Enter|Delete|[ -.]/.test(e.key))){
+    e.preventDefault()
+    
+    
+  }
+})
+
+
 function create_Order(name,price,img_url,qty){
   //product card html for front end
+  //if qty is 10 or more we will swicth from select to input
+  let qty_Select_Active
+  let qty_Input_Active
+  if (qty >= 10){
+    qty_Select_Active ='qty-select'
+    qty_Input_Active ='qty-input active'
+  } 
+  else{
+    qty_Input_Active = 'qty-input'
+    qty_Select_Active = 'qty-select active'
+  }
   return  `     <div class="product-card">
     <div class="img-box">
     <img src="${img_url}" alt="">
@@ -68,7 +110,7 @@ function create_Order(name,price,img_url,qty){
             
     </div>
     <div class="qty-wrapper">
-        <select class="qty-select" id="">
+        <select class="${qty_Select_Active}" id="">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -78,9 +120,10 @@ function create_Order(name,price,img_url,qty){
             <option value="7">7</option>
             <option value="8">8</option>
             <option value="9">9</option>
-            <option value="10">10</option>
+            <option value="10">10+</option>
             <option value="${qty}" selected disabled hidden>${qty}</option>
         </select>  
+        <input type="number" class="${qty_Input_Active}" value='${qty}'>
         <button class="remove-btn">remove</button>
     </div>
     </div>`
@@ -98,6 +141,7 @@ function setCookie(cname,cvalue,exdays){
 //define variables based on render html after its rendered
 const remove_Btn = document.querySelectorAll(".remove-btn");
 const quanity_Select = document.querySelectorAll(".qty-select");
+const quanity_Input = document.querySelectorAll(".qty-input")
 const product_Name = document.querySelectorAll(".product-name");
 const product_Card = document.querySelectorAll(".product-card");
 
@@ -135,7 +179,7 @@ remove_Btn.forEach((item,i) =>{
         } 
         //update qty and save cart in cookies by overiding cookie with same name and update shopping cart span 
       total_Qty.textContent = newCart.totalQty + " Items"
-      product_Card[i].remove()
+      product_Card[i].style.display = "none";
           const cart_String = JSON.stringify(newCart)
           setCookie('cart',cart_String, 24)
 
@@ -190,6 +234,75 @@ quanity_Select.forEach((item,i) =>{
         setCookie('cart',cart_String, 24)
         shopping_bag_qty.textContent = newCart.totalQty
         shopping_bag_qty.classList.add("active")
+    })
+})
+//same function as above for the input when over 10 qty
+quanity_Input.forEach((item,i) =>{
+    item.addEventListener('change', ()=>{
+        //make sure your for loop doesnt use i it will overlap the global varaible i in foreach statement
+        
+        let product_Cart_Price = document.querySelectorAll(".price")
+     let  qty = quanity_Input[i]
+     let current_Product_Name = product_Name[i].textContent
+        const newCart = cartQtyChange(qty.value,current_Product_Name)
+        //change cart by calling cart qty change then change the price to a string so we can make sure the number has .00 instead of 69. or 69.9
+        let product_Price = newCart.items[current_Product_Name].price.toString();
+        let checked_Product_Price = product_Price.split(".");
+       if(checked_Product_Price[1].length < 2){
+            for(x = checked_Product_Price[1].length; x<2; x++){
+                checked_Product_Price[1] = checked_Product_Price[1] +'0';
+            }
+            //split into string array and if the second part of array is less than 2 add 0 until its 2 numbers
+            checked_Product_Price = checked_Product_Price[0] + '.'+ checked_Product_Price[1]
+            product_Cart_Price[i].textContent = '$' + checked_Product_Price
+        }
+        else {
+            checked_Product_Price = checked_Product_Price[0] + "." + checked_Product_Price[1];
+            product_Cart_Price[i].textContent = '$' + checked_Product_Price;
+        } 
+        //make sure number ends in 2 numbers after decimal
+    let total_Price = newCart.totalPrice.toString()
+    let checked_Price = total_Price.split(".")
+    //change item price and total price
+    if(checked_Price[1].length < 2){
+        for(x = checked_Price[1].length; x<2; x++){
+            checked_Price[1] = checked_Price[1] +'0';
+        }
+        checked_Price = checked_Price[0] + '.'+ checked_Price[1]
+        checkout_Price.textContent = '$' + checked_Price;
+        
+    }
+    else {
+    
+        checkout_Price.textContent = '$' + newCart.totalPrice
+        
+    } 
+   //update qty and save cart in cookies by overiding cookie with same name and update shopping cart span 
+    total_Qty.textContent = newCart.totalQty + " Items"
+        const cart_String = JSON.stringify(newCart)
+        setCookie('cart',cart_String, 24)
+        shopping_bag_qty.textContent = newCart.totalQty
+        shopping_bag_qty.classList.add("active")
+    })
+})
+//switch from input or select element depeding on input size if 10 or greater make it a input if less make it a select
+quanity_Select.forEach((item,i) =>{
+    item.addEventListener('change', () =>{
+        if(item.value >= 10){
+            //the active class changes display from none to inline block
+            quanity_Select[i].classList.remove("active")
+            quanity_Input[i].classList.add("active")
+            quanity_Input[i].value = quanity_Select[i].value
+        }
+    })
+})
+quanity_Input.forEach((item,i) =>{
+    item.addEventListener('change', () =>{
+        if(item.value < 10){
+            quanity_Input[i].classList.remove("active")
+            quanity_Select[i].classList.add("active")
+            quanity_Select[i].value = quanity_Input[i].value
+        }
     })
 })
 //find cookei by name
